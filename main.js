fce5e01a03c30c62cd02ef3b044e8e4334bb8220
Monir Uddin <monir74@gmail.com>
2021-06-08 11:13:55 +0600

    details page content added

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ import './src/scss/main.scss'
 import 'bootstrap/scss/bootstrap.scss'
 
 
+const line_bar = document.getElementById('hamburger');
+line_bar.addEventListener('click', function(e) {
+    const nav_item = document.querySelector('.nav_item');
+    nav_item.classList.toggle('menu-slide');
+    line_bar.classList.toggle('cross');
+});
+
